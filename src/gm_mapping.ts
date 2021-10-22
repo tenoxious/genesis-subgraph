55fd5222236b6820dc95fe19fe792629bd79df73
tenoxious <91395140+tenoxious@users.noreply.github.com>
2021-10-21 19:34:08 -0700

    GM Transfers happen after GA Transfer. GM transers should update GA index.

--- src/gm_mapping.ts
+++ src/gm_mapping.ts
@@ -1,5 +1,5 @@
 import { Transfer as TransferEvent } from '../generated/GenesisMana/GenesisMana';
-import { Mana, Order, Bag, Transfer, Wallet } from '../generated/schema';
+import { Mana, Order, Bag, Transfer, Wallet, Adventurer } from '../generated/schema';
 import { GenesisMana } from '../generated/GenesisMana/GenesisMana';
 import { BigInt } from '@graphprotocol/graph-ts';
 
@@ -42,6 +42,29 @@ export function handleTransfer(event: TransferEvent): void {
   if (mana != null) {
     mana.currentOwner = toWallet.id;
     mana.save();
+
+    // Update GA
+    let adventurer = loadAdventurer(event, mana.inventoryId);
+    if (adventurer != null) {
+      if (0 == mana.inventoryId) {
+        adventurer.weaponGM = tokenId.toString();
+      } else if (1 == mana.inventoryId) {
+        adventurer.chestGM = tokenId.toString();
+      } else if (2 == mana.inventoryId) {
+        adventurer.headGM = tokenId.toString();
+      } else if (3 == mana.inventoryId) {
+        adventurer.waistGM = tokenId.toString();
+      } else if (4 == mana.inventoryId) {
+        adventurer.footGM = tokenId.toString();
+      } else if (5 == mana.inventoryId) {
+        adventurer.handGM = tokenId.toString();
+      } else if (6 == mana.inventoryId) {
+        adventurer.neckGM = tokenId.toString();
+      } else if (7 == mana.inventoryId) {
+        adventurer.ringGM = tokenId.toString();
+      }
+      adventurer.save();
+    }
   } else {
     mana = new Mana(tokenId.toString());
     let contract = GenesisMana.bind(event.address);
@@ -97,4 +120,16 @@ export function handleTransfer(event: TransferEvent): void {
 
 function isZeroAddress(string: string): boolean {
   return string == '0x0000000000000000000000000000000000000000';
+}
+
+function loadAdventurer(event: TransferEvent, inventoryId: i32): Adventurer | null {
+  let gaTransfer = Transfer.load(
+    event.transaction.hash.toHex() +
+      "-" +
+      event.logIndex.minus(BigInt.fromI32((inventoryId + 1) * 2)).toString()
+  );
+  if (gaTransfer == null || gaTransfer.adventurer == null) {
+    return null;
+  }
+  return Adventurer.load(gaTransfer.adventurer as string);
 }
